b68e70d345e7efdb635aac7f40554d4ed37c074e
tommycalvy <thomaslcalvy@gmail.com>
2023-06-23 20:24:13 +0000

    theme slide toggle almost done

---
+++ sveltekit/src/lib/components/slide-toggle.svelte
@@ -0,0 +1,47 @@
+<label>
+    <input type="checkbox"/>
+    <span class="slider"></span>
+</label>
+
+<style lang="postcss">
+    label {
+        position: relative;
+        display: inline-block;
+        --slide-toggle-width-half: calc(var(--slide-toggle-width) / 2);
+        width: var(--slide-toggle-width);
+        height: var(--slide-toggle-width-half);
+    }
+    input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        border-radius: var(--slide-toggle-width-half);
+        background-color: var(--color-primary);
+        transition: 0.18s;
+    }
+    .slider::before {
+        position: absolute;
+        content: "";
+        width: calc(var(--slide-toggle-width) * 2 / 5);
+        height: calc(var(--slide-toggle-width) * 2 / 5);
+        left: calc(var(--slide-toggle-width) / 20);;
+        bottom: calc(var(--slide-toggle-width) / 20);;
+        border-radius: 50%;
+        background-color: oklch(85% 0 0);
+        transition: 0.18s;
+    }
+    input:checked + .slider {
+        background-color: blue;
+    }
+    input:checked + .slider::before {
+        transform: translateX(var(--slide-toggle-width-half));
+    }
+</style>
--- sveltekit/src/routes/settings/+page.svelte
+++ sveltekit/src/routes/settings/+page.svelte
@@ -3,6 +3,7 @@
     import type { WithTarget } from "$lib/types/event-with-target";
 	import { isTheme } from "$lib/types/theme";
 	import type { PageData } from "./$types";
+	import SlideToggle from "$lib/components/slide-toggle.svelte";
 
     export let data: PageData;
 
@@ -63,66 +64,155 @@
 </script>
 
 <h1>Settings</h1>
-
+<SlideToggle />
 <h3>Theme Preferences</h3>
-<div class="theme">
+<div class="theme-preference">
     <select bind:value={colorScheme.colorMode}>
         <option value="select">Single theme</option>
         <option value="system">Sync with system</option>
     </select>
-    <label class="switch">
+    <label>
+        <span class="light-span">Light</span>
         <input type="checkbox"/>
-        <span class="slider"></span>
+        <span class="slider">
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+            <span class="icon-part"></span>
+        </span>
+        <span class="dark-span">Dark</span>
     </label>
 </div>
 
 
 <style lang="postcss">
-    .theme {
+    .theme-preference {
         display: flex;
         gap: 3rem;
     }
-    .switch {
+    label {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 1rem;
         position: relative;
-        display: inline-block;
-        width: var(--switch-width);
-        height: calc(var(--switch-width) / 2);
+        --slide-toggle-width-half: calc(var(--slide-toggle-width) / 2);
     }
-    .switch input {
-        opacity: 0;
-        width: 0;
-        height: 0;
+    input {
+        cursor: pointer;
+        margin: 0;
+        left: 0;
+        bottom: 0;
+        width: var(--slide-toggle-width);
+        height: var(--slide-toggle-width-half);
+        appearance: none;
+        border-radius: var(--slide-toggle-width-half);
+        background-color: var(--color-primary);
+        transition: 0.18s;
+    }
+    .light-span {
+        user-select: none;
+    }
+    .dark-span {
+        user-select: none;
     }
     .slider {
         position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        border-radius: calc(var(--switch-width) / 2);
-        background-color: var(--color-primary);
-        transition: 0.4s
-    }
-    .slider::before {
-        position: absolute;
         content: "";
-        width: 1.6rem;
-        height: 1.6rem;
-        left: 0.2rem;
-        bottom: 0.2rem;
+        cursor: pointer;
+        --slide-toggle-diameter: calc(var(--slide-toggle-width) * 2 / 5);
+        --slide-toggle-margin: calc(var(--slide-toggle-width) / 10);
+        width: var(--slide-toggle-diameter);
+        height: var(--slide-toggle-diameter);
+        right: 0;
         border-radius: 50%;
-        background-color: white;
-        transition: 0.4s;
+        background-color: oklch(0% 0 0);
+        transition: 0.18s;
+    }
+    input:checked {
+        background-color: grey;
     }
     input:checked + .slider {
-        background-color: blue;
+        transform: translateX(var(--slide-toggle-width-half));
     }
-    input:focus + .slider {
-        box-shadow: 0 0 1px #2196F3;
+    .icon-part {
+        position: absolute;
+        border-radius: 50%;
+        box-shadow: 0.4em -0.4em 0 0.5em hsl(0, 0%, 100%) inset;
+        top: calc(50% - 0.5em);
+        left: calc(50% - 0.5em);
+        width: 1em;
+        height: 1em;
+        transition: box-shadow var(--transDur) ease-in-out,
+            opacity var(--transDur) ease-in-out, transform var(--transDur) ease-in-out;
+        transform: scale(0.5);
     }
-
-    input:checked + .slider::before {
-        transform: translateX(2rem);
+    .icon-part ~ .icon-part {
+        background-color: hsl(0, 0%, 100%);
+        border-radius: 0.05em;
+        box-shadow: none;
+        top: 50%;
+        left: calc(50% - 0.05em);
+        transform: rotate(0) translateY(0.5em);
+        transform-origin: 50% 0;
+        width: 0.1em;
+        height: 0.2em;
+    }
+    .icon-part:nth-child(3) {
+        transform: rotate(45deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(4) {
+        transform: rotate(90deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(5) {
+        transform: rotate(135deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(6) {
+        transform: rotate(180deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(7) {
+        transform: rotate(225deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(8) {
+        transform: rotate(270deg) translateY(0.5em);
+    }
+    .icon-part:nth-child(9) {
+        transform: rotate(315deg) translateY(0.5em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(1) {
+        transform: scale(1);
+        box-shadow: 0.2em -0.2em 0 0.2em hsl(0, 0%, 100%) inset;
+    }
+    input:checked ~ .slider .icon-part ~ .icon-part {
+        opacity: 0;
+    }
+    input:checked ~ .slider .icon-part:nth-child(2) {
+        transform: rotate(45deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(3) {
+        transform: rotate(90deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(4) {
+        transform: rotate(135deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(5) {
+        transform: rotate(180deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(6) {
+        transform: rotate(225deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(7) {
+        transform: rotate(270deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(8) {
+        transform: rotate(315deg) translateY(0.8em);
+    }
+    input:checked ~ .slider .icon-part:nth-child(9) {
+        transform: rotate(360deg) translateY(0.8em);
     }
 </style>
